77d4298d957129ff564c5d4ff51b12591a00c442
¨Lifecam98¨ <tma092024@gmail.com>
2024-10-01 11:57:44 +0200

    v1.2.1

--- Program.cs
+++ Program.cs
@@ -4,25 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        //int er en variabel der vi bruker hele tall.
+        // Int er en variabel der vi bruker hele tall.
         int a = 5;
         int b = 10;
         Console.WriteLine(a + b);
 
-        //String er en variabel vi bruker for tekst.
+        // String er en variabel vi bruker for tekst.
         string name = "John Doe";
         Console.WriteLine($"Hello {name}, how are you?");
 
-        //Double er en variabel vi bruker for tall både med og uten desimaler.
+        // Double er en variabel vi bruker for tall både med og uten desimaler.
         double pi = 3.14159;
         Console.WriteLine($"The value of pi is approximately {pi}");
 
-        //Char er en variabel vi bruker for å representere en enkelt tegn.
+        // Char er en variabel vi bruker for å representere en enkelt tegn.
         char letter = 'A';
         Console.WriteLine($"The ASCII value of '{letter}' is {Convert.ToInt32(letter)}");
 
+        // Arrays, store multiple values av samme datatype
+        int[] myIntArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
+        // Loop igjennom array, bruker index-baserte iteratore
+        // Iterator++ er det samme som: Iterator = iterator + 1;
+        for (int iterator = 0; iterator < myIntArray.Length; iterator++)
+        {
+            Console.WriteLine ("$Iterator on position: {iterator}, index of the numbers in the array: {myIntArray[iterator]}");
+        }
 
-        Console.WriteLine("Hello, World!");
+        // Vi assigner en ny string array, og vi bruker string[] myStringArray = {"some", "strings"};
+        string[] myStringArray = { "Hello", "From", "A", "String", "Array" };
+
+        // Vi kan bruke en foreach loop, for å loope gjennom string arrayen med en enkelt string variabel.
+        // string[] er et array, (string myStrings) er en singel variabel, og in myStringArray er en iterator.
+        foreach (string myStrings in myStringArray)
+        {
+            Console.WriteLine(myStrings);
+        }
+
+        // Fortsett her!
     }
 }
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c15deb020f7d58991822bed309356d7e22328c58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6518049a063067176910c35083d9c466ea130443")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
