c15deb020f7d58991822bed309356d7e22328c58
¨Lifecam98¨ <tma092024@gmail.com>
2024-10-01 08:59:55 +0200

    v1.1.3

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        int a = 5;
+        int b = 10;
+        Console.WriteLine(a + b);
         Console.WriteLine("Hello, World!");
     }
 }
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5d896a6a382863c04ec1c520fe6c919238ad123")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7698f033d572a991256338274e6437db4d266112")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
