e3fa164ec9902c596ff988a526d6daac7345ce4d
¨Lifecam98¨ <tma092024@gmail.com>
2024-10-03 13:07:23 +0200

    v1.2.5

--- Program.cs
+++ Program.cs
@@ -8,39 +8,58 @@ class Program
         int a = 5;
         int b = 10;
         Console.WriteLine(a + b);
-
+        
         // String er en variabel vi bruker for tekst.
         string name = "John Doe";
         Console.WriteLine($"Hello {name}, how are you?");
-
+        
         // Double er en variabel vi bruker for tall både med og uten desimaler.
         double pi = 3.14159;
         Console.WriteLine($"The value of pi is approximately {pi}");
-
+        
         // Char er en variabel vi bruker for å representere en enkelt tegn.
         char letter = 'A';
         Console.WriteLine($"The ASCII value of '{letter}' is {Convert.ToInt32(letter)}");
-
+        
         // Arrays, store multiple values av samme datatype
         int[] myIntArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-
+        
         // Loop igjennom array, bruker index-baserte iteratore
         // Iterator++ er det samme som: Iterator = iterator + 1;
         for (int iterator = 0; iterator < myIntArray.Length; iterator++)
         {
             Console.WriteLine ($"Iterator on position: {iterator}, index of the numbers in the array: {myIntArray[iterator]}");
         }
-
+        
         // Vi assigner en ny string array, og vi bruker string[] myStringArray = {"some", "strings"};
         string[] myStringArray = {"Hello", "From", "A", "String", "Array"};
-
+        
         // Vi kan bruke en foreach loop, for å loope gjennom string arrayen med en enkelt string variabel.
         // string[] er et array, (string myStrings) er en singel variabel, og in myStringArray er en iterator.
         foreach (string myStrings in myStringArray)
         {
             Console.WriteLine(myStrings);
         }
-
+        
         // Fortsett her!
+        //Lager en ny List med strings
+        List<string> myList = new List<string>();
+        
+        //Legger til strings i listen
+        myList.Add("En");
+        myList.Add("To");
+        myList.Add("Tre");
+        myList.Add("Fire");
+        myList.Add("Fem");
+        
+        //Legger til en ny array av strings i listen
+        string[] moresStrings = {"Seks", "Sju", "Åtte", "Ni", "Ti"};
+        myList.AddRange(moresStrings);
+        
+        //Loop gjennom listen med en foreach loop
+        foreach (string myStrings in myList)
+        {
+            Console.WriteLine(myStrings);
+        }
     }
 }
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdd54e403ea2351b4268ce7debe0b50e761ad245")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c23d4f7b910f35127da305f30ed47c914dce776")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
